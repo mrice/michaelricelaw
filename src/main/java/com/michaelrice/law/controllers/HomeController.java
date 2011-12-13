9894a422356ec3472167aa4d1bdd790582d1c96c
Michael Rice <me@michaelrice.com>
2011-12-12 23:07:05 -0800

    initial files

---
+++ src/main/java/com/michaelrice/law/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.michaelrice.law.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	@RequestMapping(value={"/","/home"}, method=RequestMethod.GET)
+	public ModelAndView generateHomePage() {
+		return new ModelAndView("home");
+	}
+}
